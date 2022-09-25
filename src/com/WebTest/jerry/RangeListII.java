c78f05a7791dbe6f693cfa3b45bbd7928e81955c
MC-Zealot <263097241@qq.com>
2022-09-25 16:42:46 +0800

    range list

--- src/com/WebTest/jerry/RangeListII.java
+++ src/com/WebTest/jerry/RangeListII.java
@@ -10,52 +10,63 @@ public class RangeListII {
             this.rangeEnd = rangeEnd;
         }
 
-        private ListNode unlink(ListNode element){
-            if(element.rangeEnd - element.rangeStart <= 0){
+        private ListNode removeNode(ListNode node){
+            if(node.rangeEnd - node.rangeStart <= 0){
                 return this;
             }
-            if(element.rangeStart > this.rangeEnd){
+            if(node.rangeStart > this.rangeEnd){
                 if(this.next!= null){
-                    this.next = this.next.unlink(element);
+                    this.next = this.next.removeNode(node);
                 }
                 return this;
             }
 
-            if(this.rangeStart == element.rangeStart && this.rangeEnd == element.rangeEnd){
+            if(this.rangeStart == node.rangeStart && this.rangeEnd == node.rangeEnd){
                 return this.next;
-            }else if(this.rangeStart < element.rangeStart && this.rangeEnd >= element.rangeEnd){
-                ListNode leftNode = new ListNode(this.rangeStart, element.rangeStart);
-                ListNode rightNode = new ListNode(element.rangeEnd, this.rangeEnd);
+            }else if(this.rangeStart < node.rangeStart && this.rangeEnd >= node.rangeEnd){
+                ListNode leftNode = new ListNode(this.rangeStart, node.rangeStart);
+                ListNode rightNode = new ListNode(node.rangeEnd, this.rangeEnd);
                 rightNode.next = this.next;
                 leftNode.next = rightNode;
                 return leftNode;
             }else{
-                while(this.next != null && element.rangeEnd > this.next.rangeEnd){
+                while(this.next != null && node.rangeEnd > this.next.rangeEnd){
                     this.next = this.next.next;
                 }
                 if(this.next!=null){
-                    this.rangeEnd = element.rangeStart;
-                    this.next.rangeStart = element.rangeEnd;
+                    this.rangeEnd = node.rangeStart;
+                    this.next.rangeStart = node.rangeEnd;
                 }else{
-                    this.rangeStart = element.rangeEnd;
+                    this.rangeStart = node.rangeEnd;
                 }
                 return this;
             }
         }
 
-        private ListNode link(ListNode element){
-            if(this.rangeStart <= element.rangeStart && this.rangeEnd >= element.rangeEnd){
-                return this;
-            }else if(this.rangeStart > element.rangeEnd){
-                element.next = this;
-                return element;
-            }else{
-                if(this.rangeEnd < element.rangeStart){
-                    this.next = this.next != null?this.next.link(element):element;
-                }else{
-                    this.rangeStart = this.rangeStart < element.rangeStart? this.rangeStart: element.rangeStart;
-                    this.rangeEnd = this.rangeEnd > element.rangeEnd? this.rangeEnd : element.rangeEnd;
+        /**
+         * Add a new node to the range list
+         * @param node new node to be added
+         * @return
+         */
+        private ListNode addNode(ListNode node){
 
+            if(this.rangeStart <= node.rangeStart && this.rangeEnd >= node.rangeEnd){
+                //if new node is already included, return
+                return this;
+            } else if(this.rangeStart > node.rangeEnd){
+                //if range end of new node is before current range
+                //new node go into the head
+                node.next = this;
+                return node;
+            }else{
+                if(this.rangeEnd < node.rangeStart){
+                    //if range start of new node is after current range, try next range recursively
+                    this.next = this.next != null?this.next.addNode(node):node;
+                }else{
+                    //if The two ranges overlap, combine them into new range
+                    this.rangeStart = this.rangeStart < node.rangeStart? this.rangeStart: node.rangeStart;
+                    this.rangeEnd = this.rangeEnd > node.rangeEnd? this.rangeEnd : node.rangeEnd;
+                    //if new range and next range overlap, combine them
                     while(this.next != null && this.rangeEnd >= this.next.rangeStart){
                         this.rangeEnd = this.next.rangeEnd;
                         this.next = this.next.next;
@@ -67,25 +78,27 @@ public class RangeListII {
     }
     private ListNode head = null;
     /**
-     *  Adds a range to the list * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     * Adds a range to the list
+     * @param range Array of two integers that specify beginning and end of range.
      */
     public void add(int[] range) {
         ListNode newNode = new ListNode(range[0], range[1]);
         if(this.head == null){
             this.head = newNode;
         }else{
-            this.head = this.head.link(newNode);
+            this.head = this.head.addNode(newNode);
         }
     }
-    /** *
-     * Removes a range from the list * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-     * 没有说是否需要返回
-     * */
+
+    /**
+     * Removes a range from the list
+     * @param range  Array of two integers that specify beginning and end of range.
+     */
     public void remove(int[] range) {
         if(this.head == null){
             return;
         }
-        this.head = this.head.unlink(new ListNode(range[0], range[1]));
+        this.head = this.head.removeNode(new ListNode(range[0], range[1]));
     }
 
     /** * Prints out the list of ranges in the range list */
